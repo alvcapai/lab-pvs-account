391b56e473a0a056957d7d1a709367ea5a329be6
Álvaro Capai <Alvaro.Capai@ibm.com>
2025-04-09 21:26:39 -0300

    xxx

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    ibm = {
+      source  = "ibm-cloud/ibm"
+      version = ">= 1.49.0"
+    }
+  }
+}
+
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.powervs_zone
